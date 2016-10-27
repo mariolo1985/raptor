e0d302df47840dca484fdf2923424937b040a823
mariolo1985 <loserpunkx182@gmail.com>
2016-10-27 15:08:28 -0600

    updated to bring in attachment/dropzone

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Attachment from '../build/Attachment.js';
+
+render(
+    (
+        <Attachment />
+    ),
+    document.getElementById('test')
+);
