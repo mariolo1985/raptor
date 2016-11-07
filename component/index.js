1403e6f7c26154f2d02d98a3ba640441cac86fe8
mariolo1985 <loserpunkx182@gmail.com>
2016-11-07 16:59:27 -0700

    added components

--- component/index.js
+++ component/index.js
@@ -1,4 +1,9 @@
+export MainNav from './Header/MainNav';
+
 export Attachment from './Drop/Attachment';
 export FilePreview from './Drop/FilePreview';
 
 export EditorBox from './ContentEditor/EditorBox';
+
+export FileDisplay from './FileDisplay/FileDisplay';
+export DisplaySet from './FileDisplay/DisplaySet';
