488127ad24f145255c28cc25bf9b18da96175940
mariolo1985 <loserpunkx182@gmail.com>
2016-11-07 16:56:18 -0700

    updated service to create a metadata json file

--- services/filehelper.php
+++ services/filehelper.php
@@ -3,16 +3,68 @@
 class FileHelper
 {
     function __construct(){
-        echo 'filehelper constructed';
+
     }
 
-    function putFile($files){        
+    function putFile($files){   
+        $guid = uniqid("raptor_");     
+        $moveToPath = '../pending_elements/' . $guid . '/';
+        
+        $combineFiles=array();// used to hold the different types of files
+
         foreach ($files as $key => $value){
             $_name = $files[$key]['name'];
             $_size = $files[$key]['size'];
-            $_tmpname = $files[$key]['tmp_name'];
-            $result = move_uploaded_file($_tmpname,'../pending_elements/' . $_name);
+            $_tmpname = $files[$key]['tmp_name'];      
+                 
+            $combineFiles[] = array(
+                    'filename' => $_name
+            );
+            
+            // CHCK IF DIRECTORY EXIST
+            if (!is_dir($moveToPath)){
+                mkdir($moveToPath);
+            }
+            $result = move_uploaded_file($_tmpname,$moveToPath . $_name);
+            if (!$result){
+                // MOVED ERROR
+                $guid = null;
+            }
+        }// end foreach
+
+        file_put_contents($moveToPath.'metadata.json',json_encode($combineFiles));// add metadata file
+
+        return $guid;
+    }
+
+    // GET PENDING FILE NAMES BY SET
+    function getPendingFilesBySetId($setId){
+        $path = '../pending_elements/' . $setId;
+        $filePath = $path . '/metadata.json';
+        if (file_exists($filePath)){
+            $fileContent = file_get_contents($filePath);
+            return $fileContent;
+        }else{
+            return null;
         }
+    }
+
+    function getPendingFilesMetadata($sets){
+        $PendingSets = array();
+
+        foreach ($sets as $key => $set){            
+            $setId = $set['setId'];
+            $uploadDate = $set['UploadDate'];
+            $tmp = json_decode($this->getPendingFilesBySetId($setId),true);
+            
+            // ADD UPLOAD DATE TO DATA
+            $tmp[] = array(
+                'Upload Date'=>$uploadDate
+            );
+            $PendingSets[]=json_encode($tmp);                     
+        }
+
+        return $PendingSets;
     }
 }
 
--- services/putfile.php
+++ services/putfile.php
@@ -3,12 +3,13 @@
 * THIS IS A WEB SERVICE TO PUT A FILE IN DIRECTORY
 * VIA HELPER CLASS
 */
+
 require 'filehelper.php';
 header("Content-type: text/plain");
 
-
 $_fileHelper = new FileHelper();
-$_fileHelper->putFile($_FILES);
+$guid = $_fileHelper->putFile($_FILES);
+echo $guid;
 
 
 ?>
