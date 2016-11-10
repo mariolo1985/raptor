d524cd777e75928dfeecf370107a254bfa05a40d
mariolo1985 <loserpunkx182@gmail.com>
2016-11-09 17:07:29 -0700

    moved editor box code on render

--- component/Drop/Attachment.js
+++ component/Drop/Attachment.js
@@ -14,6 +14,7 @@ class Attachment extends Component {
 
         this.onDrop = this.onDrop.bind(this);
         this.postFiles = this.postFiles.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
     }
     onDrop(results) {
         this.setState({
@@ -25,28 +26,19 @@ class Attachment extends Component {
         for (var i = 0; i < this.state.files.length; i++) {
             fd.append('file_' + i + 1, this.state.files[i]);
         }
-        putFile(fd);
+        //putFile(fd);
+        console.log($('.editor').html());
 
+    }
+    componentDidMount() {
+        createEditor();
     }
     render() {
         var highDetails = this.state.files.length + " File(s) To Be Checked-In";
+        var checkinClass = this.state.files.length > 0 ? "btn btn-checkin" : "btn btn-checkin disabled";
         return (
 
             <div className='attachment-wrapper'>
-                <div className='attachment-zone'>
-                    <h1 className='center-helper'>Attach New Elements For Preview</h1>
-                    <Dropzone className='attachments' activeClassName="dragon" onDrop={this.onDrop} disableClick multiple>
-                        <div className='default-msg'>
-                            <p className='attachment-instructions'>Drop Files Here</p>
-                            <i className='attachment-flair fa fa-anchor' />
-                        </div>
-                        <div className='dragged-msg'>
-                            <p className='attachment-instructions'>Drop to upload</p>
-                            <i className='attachment-flair fa fa-telegram' />
-                        </div>
-                    </Dropzone>
-                </div>
-
                 {this.state.files.length > 0 ?
                     <div className='attachment-preview clear'>
                         <h2 className='high-details'>{highDetails}</h2>
@@ -60,9 +52,32 @@ class Attachment extends Component {
                             )
                         }
                         )}
-                        <EditorBox onPostFiles={this.postFiles} />
                     </div>
-                    : null}
+                    :
+                    <div className='attachment-zone'>
+                        <h1 className='center-helper'>Attach New Elements For Preview</h1>
+                        <Dropzone className='attachments' activeClassName="dragon" onDrop={this.onDrop} disableClick multiple>
+                            <div className='default-msg'>
+                                <p className='attachment-instructions'>Drop Files Here</p>
+                                <i className='attachment-flair fa fa-anchor' />
+                            </div>
+                            <div className='dragged-msg'>
+                                <p className='attachment-instructions'>Drop to upload</p>
+                                <i className='attachment-flair fa fa-telegram' />
+                            </div>
+                        </Dropzone>
+                    </div>
+                }
+
+                <div className='attachment-editor-wrapper'>
+                    <h3>Enter Check-In Comments:</h3>
+                    <div className='toolbar'></div>
+                    <div className='editor'></div>
+                    <div className='action-buttons'>
+                        <button className='btn btn-reset'>Reset Form</button>
+                        <button className={checkinClass} onClick={this.postFiles}>Check In</button>
+                    </div>
+                </div>
             </div>
         )
     }
