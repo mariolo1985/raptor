bd4f8de2300c1b1613cd66452014a4a922f1e22e
mariolo1985 <loserpunkx182@gmail.com>
2016-11-10 13:55:41 -0700

    builds

--- build/index.js
+++ build/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.DisplaySet = exports.FileDisplay = exports.EditorBox = exports.FilePreview = exports.Attachment = exports.MainNav = undefined;
+exports.DisplaySet = exports.FileDisplay = exports.FilePreview = exports.Attachment = exports.MainNav = undefined;
 
 var _MainNav2 = require('./Header/MainNav');
 
@@ -16,10 +16,6 @@ var _Attachment3 = _interopRequireDefault(_Attachment2);
 var _FilePreview2 = require('./Drop/FilePreview');
 
 var _FilePreview3 = _interopRequireDefault(_FilePreview2);
-
-var _EditorBox2 = require('./ContentEditor/EditorBox');
-
-var _EditorBox3 = _interopRequireDefault(_EditorBox2);
 
 var _FileDisplay2 = require('./FileDisplay/FileDisplay');
 
@@ -34,6 +30,5 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 exports.MainNav = _MainNav3.default;
 exports.Attachment = _Attachment3.default;
 exports.FilePreview = _FilePreview3.default;
-exports.EditorBox = _EditorBox3.default;
 exports.FileDisplay = _FileDisplay3.default;
 exports.DisplaySet = _DisplaySet3.default;
