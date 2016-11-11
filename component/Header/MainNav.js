9ed4b11572205b62c4522b59ad4a3f87f7e663b0
mariolo1985 <loserpunkx182@gmail.com>
2016-11-11 16:57:55 -0700

    updated icons and element arragement

--- component/Header/MainNav.js
+++ component/Header/MainNav.js
@@ -5,35 +5,41 @@ class MainNav extends Component {
         super(props);
 
     }
+    componentDidMount(){
+
+        var path = window.location.pathname;
+        var index = path.lastIndexOf("/")+1;''
+        var pageUrl = path.substring(index);
+
+        $('.menu-item .menu-link[href*="' + pageUrl + '"]').parents('.menu-item').addClass('selected');
+    }
     render() {
         return (
             <section className='header-wrapper clear'>
                 <div className='logo-header header-content'>
+                    <div className='title-wrapper'>
+                        <span className='header-title'>RAPTOR</span>
+                    </div>
                     <div className='logo'>
                         <a href='/raptor' className='header-logo-link'>
                             <img src='./images/raptor.png' className='header-img' alt='raptor.com' />
-                        </a>
-                        <span>RAPTOR</span>
+                        </a>                        
                     </div>
                 </div>
+
                 <div className='menu-wrapper header-content'>
                     <ul className='menu clear'>
                         <li className='menu-item'>
-                            <a href='./index.html' className='menu-link'>Add Elements</a>
+                            <a href='./index.html' className='menu-link'><img className='menu-icon' src='./images/addelementicon.png'/></a>
                         </li>
                         <li className='menu-item'>
-                            <a href='./review.html' className='menu-link'>Review</a>
+                            <a href='./review.html' className='menu-link'><img className='menu-icon' src='./images/reviewicon.png'/></a>
                         </li>
                         <li className='menu-item'>
-                            <a href='./history.html' className='menu-link'>History</a>
+                            <a href='./history.html' className='menu-link'><img className='menu-icon' src='./images/historyicon.png'/></a>
                         </li>
 
                     </ul>
-                </div>
-                <div className='search-wrapper header-content'>
-                    <button className='btn-search'>
-                        <i className='fa fa-search'></i>
-                    </button>
                 </div>
             </section>
         )
