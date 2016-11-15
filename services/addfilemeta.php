16b7959bd877b82f55ae7459201f215a2e5ff5cb
mariolo1985 <loserpunkx182@gmail.com>
2016-11-14 17:15:40 -0700

    updated to post a wf status

--- services/addfilemeta.php
+++ services/addfilemeta.php
@@ -1,12 +1,14 @@
 <?php
 include "dbhelper.php";
 
-$postSetId = $_POST['SetId'];
-
 if (isset($_POST['SetId'])){
     $_dbhelper = new DbHelper;
     $conn = $_dbhelper->getDefaultConnected();
-    $result = $_dbhelper->insertToPendingElements($conn,$postSetId, date("Y-m-d H:i:s"));
+
+    $postSetId = $_POST['SetId'];
+    $postStatus = $_POST['Status'];
+
+    $result = $_dbhelper->insertToPendingElements($conn,$postSetId, date("Y-m-d H:i:s"),$postStatus);
     echo $result;
 }
 
