62d37bb91fed04ba2ac048943fa8a194359efea6
mariolo1985 <loserpunkx182@gmail.com>
2016-11-16 16:33:09 -0700

    Removed web service call. Call getPendingElements on base and hit callback on reviews.js

--- js/review.js
+++ js/review.js
@@ -8,53 +8,6 @@ render(
     ),
     document.getElementById('main-nav')
 );
-getPendingElements();
-
-
-
-
-// GETS PENDING ELEMENTS
-function getPendingElements() {
-
-    $.ajax({
-        //  THIS CALLS THE DB ON PENDING ELEMENTS TABLE
-        url: './services/getfiles.php',
-        type: 'GET',
-        dataType: 'json',
-        data: {
-            Filter: "PENDING_ELEMENTS"
-        }
-    })
-        .done(function (result) {
-            $.ajax({
-                // GET EACH SET FILE IN A SET
-                url: './services/getfiles.php',
-                type: 'GET',
-                dataType: 'json',
-                data: {
-                    Filter: "BY_SETS",
-                    Sets: result
-                }
-            })
-                .done(function (result) {
-                    renderElements(result);
-                })
-                .fail(function (a, b, c) {
-                    console.log('Failed BY_SETID POST');
-                    console.log(a);
-                    console.log(b);
-                    console.log(c);
-                });
-        })
-        .fail(function (a, b, c) {
-            console.log('Failed PENDING_ELEMENTS POST');
-            console.log(a);
-            console.log(b);
-            console.log(c);
-        })
-
-}
-
 
 // COMPONENT
 function renderElements(elements) {
@@ -67,3 +20,4 @@ function renderElements(elements) {
     );
 }
 
+getPendingElements(renderElements);
