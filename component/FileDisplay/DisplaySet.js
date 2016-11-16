16cb554be12e3a553b0d5a2086aeee734938636b
mariolo1985 <loserpunkx182@gmail.com>
2016-11-16 16:31:02 -0700

    Moved service calls out. Passes an update callback to child component *refresh*. Pass callback to get pending elements service call

--- component/FileDisplay/DisplaySet.js
+++ component/FileDisplay/DisplaySet.js
@@ -3,32 +3,59 @@ import React, { Component } from 'react';
 class DisplaySet extends Component {
     constructor(props) {
         super(props);
+
+        this.updateWorkflowStatus = this.updateWorkflowStatus.bind(this);        
     }
     createMarkup(html) {
+        // CREATE MARKUP FOR HTML INJECTION
         return { __html: html };
     }
-    render() {
-        var jSet = JSON.parse(this.props.set);// JSON THE SET RESULTS
+    makeDate(uploadDate){
+        // PARSE DATE AND RETURN AS READABLE STRING
+        uploadDate = new Date(uploadDate);
+
+        var hours = uploadDate.getHours();
+        var minutes = uploadDate.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+
+        uploadDate = (uploadDate.getMonth() + 1) + '/' + uploadDate.getDate() + '/' + uploadDate.getFullYear() + ' ' + strTime;
+
+        return uploadDate;
+    }
+    updateWorkflowStatus(setid,status){
+        // DB UPDATE SUCCESS        
+        $.ajax({
+            url:'./services/updatestatus.php',
+            type:'POST',
+            data:{
+                SetId:setid,
+                Status:status
+            }
+        })
+        .done(function(result){
+            this.props.refresh();
+        }.bind(this));
+    }
+    parseSetJson(jSet){
         var uploadDate,
             setId,
-            comments = "";
-        var filenames = [];
+            wfStatus,
+            comments = "",
+            filenames = [];
+
         jSet.map((item, i) => {
             if (item['Upload Date']) {
                 // PARSE UPLOAD DATE IN READABLE FORMAT
                 uploadDate = item['Upload Date'];
+                uploadDate = this.makeDate(uploadDate);
 
-                uploadDate = new Date(uploadDate);
-                var hours = uploadDate.getHours();
-                var minutes = uploadDate.getMinutes();
-                var ampm = hours >= 12 ? 'pm' : 'am';
-                hours = hours % 12;
-                hours = hours ? hours : 12; // the hour '0' should be '12'
-                minutes = minutes < 10 ? '0' + minutes : minutes;
-                var strTime = hours + ':' + minutes + ' ' + ampm;
-
-                uploadDate = (uploadDate.getMonth() + 1) + '/' + uploadDate.getDate() + '/' + uploadDate.getFullYear() + ' ' + strTime;
-
+            }else if (item['WorkflowStatus']){  
+                // GET WORKFLOW STATUS
+                wfStatus = item['WorkflowStatus'];
             } else if (item['Comments']) {
                 // GET COMMENTS WHEN FILES WERE UPLOADED
                 var tempComments;
@@ -46,10 +73,44 @@ class DisplaySet extends Component {
             }
         });
 
+
+         var SetInfo = {
+            'SetId':setId,
+            'UploadDate':uploadDate,
+            'WorkflowStatus':wfStatus,
+            'Comments':comments,
+            'filenames':filenames
+        }
+
+        return SetInfo;
+
+    }
+    render() {
+        var jSet;
+        jSet = JSON.parse(this.props.set);
+
+        var info = this.parseSetJson(jSet);
+        var uploadDate,
+            setId,
+            wfStatus,
+            comments = "",
+            filenames = [];
+
+        uploadDate = info['UploadDate'];
+        setId = info['SetId'];
+        wfStatus = info['WorkflowStatus'];
+        comments = info['Comments'];
+        filenames = info['filenames'];
+
         var setTitle = "Uploaded: " + uploadDate;
+        var isPending = wfStatus=="PENDING"? true:false;
+        var statusRowClass= isPending ? 'file-status-row pending' : 'file-status-row approved';
         return (
             <div className='file-set-container' data-setid={setId}>
                 <div className='file-row clear'>
+                    <div className={statusRowClass}>
+                        <span className='file-status'>{wfStatus}</span>
+                    </div>
                     <div className='file-set'>
                         <p className='file-set-title'>{setTitle}</p>
                         {
@@ -70,7 +131,12 @@ class DisplaySet extends Component {
                     </div>
                     <div className='file-action'>
                         <button className='btn btn-reset'>Reject</button>
-                        <button className='btn btn-checkin'>Approve</button>                        
+                        {
+                            isPending ?
+                                <button className='btn btn-checkin' onClick={() => this.updateWorkflowStatus(setId,'APPROVED')}>Approve</button>          
+                                : 
+                                <button className='btn btn-checkin' onClick={() => this.updateWorkflowStatus(setId,'IMPLEMENTED')}>Implement</button>   
+                        }             
                     </div>
                 </div>
             </div>
--- component/FileDisplay/FileDisplay.js
+++ component/FileDisplay/FileDisplay.js
@@ -5,17 +5,32 @@ import { DisplaySet } from '../';
 class FileDisplay extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            sets:[]
+        }
+
+        this.getPendingElements = this.getPendingElements.bind(this);
+        this.updateSets = this.updateSets.bind(this);
+
+        this.getPendingElements();// GET PENDING ELEMENTS
+    }
+    getPendingElements(){
+        getPendingElements(this.updateSets);
+    }    
+    updateSets(sets){
+        this.setState({
+            sets: sets
+        })
     }
     render() {
-
         return (
             <div className='file-display-wrapper'>
-                <h1>({this.props.sets.length}) Pending Sets In Review</h1>
+                <h1>({this.state.sets.length}) Pending Sets In Review</h1>
                 {
-                    this.props.sets.length > 0 ?
-                     this.props.sets.map((set, i) => {
+                    this.state.sets.length > 0 ?
+                     this.state.sets.map((set, i) => {
                         return (
-                            <DisplaySet set={set} key={i} />
+                            <DisplaySet set={set} key={i} refresh={this.getPendingElements}/>
                         );
                     })
                      : null
