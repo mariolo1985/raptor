24752097086f0f8d9e36a2c442d86949980fb628
mariolo1985 <loserpunkx182@gmail.com>
2016-11-30 11:50:50 -0700

    Removed sub-comp that are only used in main components

--- component/index.js
+++ component/index.js
@@ -1,7 +1,4 @@
 export MainNav from './Header/MainNav';
-
 export Attachment from './Drop/Attachment';
-export FilePreview from './Drop/FilePreview';
-
 export FileDisplay from './FileDisplay/FileDisplay';
-export DisplaySet from './FileDisplay/DisplaySet';
+export ImpFileDisplay from './FileDisplay_Imp/ImpFileDisplay';
