b4c66ef11409d48040fe6ecf857cf7b46761d747
mariolo1985 <loserpunkx182@gmail.com>
2016-11-30 11:55:11 -0700

    fixed connection issues - next_result()

--- services/updatestatus.php
+++ services/updatestatus.php
@@ -3,20 +3,38 @@
 if (isset($_POST['SetId'])){
     include "dbhelper.php";
 
-    $_dbHelper = new DbHelper();
-    $conn = $_dbHelper->getDefaultConnected();
+    $_dbhelper = new DbHelper();
+    $conn = $_dbhelper->getDefaultConnected();
 
     $setid = $_POST['SetId'];
     $status = $_POST['Status'];
 
-    $updatedPendingElement = $_dbHelper->updatePendingElementStatus($conn, $setid,$status);
+    $updatedPendingElement = $_dbhelper->updatePendingElementStatus($conn, $setid,$status);
     if (($updatedPendingElement) && ($status == 'IMPLEMENTED')){
+        // ELEMETS HAVE BEEN IMPLEMENTED INTO CURRENT PATTERNS
+
         include "filehelper.php";
         $_filehelper = new FileHelper();
-        $result = $_filehelper->moveImplementedFiles($setid);
+        $result = $_filehelper->moveImplementedFiles($setid); // MOVE FILES FROM [SETID] DIRECTORY
+
+        // INCREMENT MINOR VERSION
+        $conn->next_result();
+        $curVersionNum = $_dbhelper->getMainVersionNumber($conn);// current version 
+        $newVersionNum = floatval($curVersionNum) + 0.01;
+
+        $conn->next_result();
+        $isSet = $_dbhelper->insertSetVersion($conn, $setid,$newVersionNum, date("Y-m-d H:i:s"));
+        if ($isSet){
+            // SUCCESSFULLY INCREMENTED
+            $conn->next_result();
+            $updateSuccess = $_dbhelper->updateVersionNumber($conn,$newVersionNum);
+        }
+
     }
+
     echo $updatedPendingElement ? 'TRUE':'FALSE';
-    
+
+    $conn->close();
 }
 
 ?>
