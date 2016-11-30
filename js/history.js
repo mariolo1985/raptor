5e68b93087693ed72f2eff3081d6f3b40dc42c9d
mariolo1985 <loserpunkx182@gmail.com>
2016-11-30 11:52:46 -0700

    history page

--- js/history.js
+++ js/history.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {MainNav} from '../build';
+import {MainNav, ImpFileDisplay} from '../build';
 
 render(
     (
@@ -8,3 +8,34 @@ render(
     ),
     document.getElementById('main-nav')
 );
+
+function getImplementedJson(done_callback){
+    $.ajax({
+        url:'./services/getfiles.php',
+        type:'GET',
+        data:{
+            Filter:'BY_ELEMENT_VERSION'
+        }
+    })
+    .done(function(result){
+        done_callback(result);
+    })
+    .fail(function(a,b,c){
+        console.log(a);
+        console.log(b);
+        console.log(c);
+    });
+}
+
+$(document).ready(function(){
+    getImplementedJson(renderSets);
+});
+
+function renderSets(Sets){
+    render(
+        (
+            <ImpFileDisplay Sets={Sets} />
+        ),
+        document.getElementById('file-display')
+    )
+}
